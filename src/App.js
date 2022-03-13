f50636ab7e6be52073389ad70521f2dc0e9dd0f2
subhramukherjee2398 <subhramukherjee560@gmail.com>
2022-03-14 02:49:16 +0530

    add all product and select a product details

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 
-import './App.css';
+import Header from "./containers/Header";
+import ProductComponent from "./containers/ProductComponent";
+import ProductDeatils from "./containers/ProductDeatils";
+import ProductListing from "./containers/ProductListing";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>Hello Redux Axios</div>
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          {/* <Route path="" element={<ProductComponent />} /> */}
+          <Route path="/" exact element={<ProductListing />} />
+          <Route path="/product/:productId" element={<ProductDeatils />} />
+          <Route>404 not found</Route>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
